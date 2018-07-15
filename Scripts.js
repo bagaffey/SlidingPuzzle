0adf84112062a419252a9f823e997829e34ee7b3
Barry Gaffey <bagaffey@users.noreply.github.com>
2018-07-15 11:08:35 -0400

    Updates Scripts.js
    The quest to be hip.

--- Scripts.js
+++ Scripts.js
@@ -6,4 +6,5 @@
   const clickScore = document.getElementById('clicks');
   const soundButton = document.getElementById('toggle-audio');
   const congratulations = document.getElementsByClassName('congratulations')[0];
+  let shuffledPieces = pieces;
 }());
