5ec7c69cf72ae70faa5749b26a23eb457b96c84c
Barry Gaffey <bagaffey@users.noreply.github.com>
2018-08-20 11:46:32 -0400

    Updated Scripts.js
    Happiness.

--- Scripts.js
+++ Scripts.js
@@ -226,6 +226,10 @@
         currentState(true);
         break;
       }
+      case "h": {
+        whereIsHiddenPiece(true);
+        break;
+      }
       case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9": {
         const clickedPiece = document.getElementById(`piece-${parseInt(event.key)}`);
         indexOfClickedPiece = shuffledPieces.indexOf(clickedPiece);
