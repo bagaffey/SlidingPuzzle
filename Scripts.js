d9ee76c5dd4741fbe72c737b2cd7e7163a3e3d5b
Barry Gaffey <bagaffey@users.noreply.github.com>
2018-08-28 14:24:37 -0400

    Updated Scripts.js
    Set the fog distance.

--- Scripts.js
+++ Scripts.js
@@ -257,6 +257,7 @@
     window.currentlyPlaying = audioVar.target;
   };
 
-  addEventListener('keydown', handleKeydown);
-  addEventListener('play', playAudio, true);
+  addEventListener("keydown", handleKeydown);
+  addEventListener("play", playAudio, true);
+  soundButton.addEventListener("click", toggleAudio);
 }());
