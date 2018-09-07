2e6d8ccb9a95801a7fab391ffb71d70e17b494dd
Barry Gaffey <bagaffey@users.noreply.github.com>
2018-09-06 22:27:31 -0400

    Updated Scripts.js
    A sea of debt.

--- Scripts.js
+++ Scripts.js
@@ -3,7 +3,7 @@
   'use strict';
   
   const pieces = [...document.querySelectorAll(".puzzle-game > img")];
-  const clickScore = document.getElementById('clicks');
+  const clickScore = document.getElementById("clicks");
   const soundButton = document.getElementById('toggle-audio');
   const congratulations = document.getElementsByClassName('congratulations')[0];
   let shuffledPieces = pieces;
