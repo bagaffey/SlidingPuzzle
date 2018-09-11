0d9a7951ffe743748d6364be84610f23ab244f81
Barry Gaffey <bagaffey@users.noreply.github.com>
2018-09-11 13:09:08 -0400

    Updated Scripts.js
    Cognitive dissonance.

--- Scripts.js
+++ Scripts.js
@@ -4,7 +4,7 @@
   
   const pieces = [...document.querySelectorAll(".puzzle-game > img")];
   const clickScore = document.getElementById("clicks");
-  const soundButton = document.getElementById('toggle-audio');
+  const soundButton = document.getElementById("toggle-audio");
   const congratulations = document.getElementsByClassName("congratulations")[0];
   let shuffledPieces = pieces;
   const hiddenPiece = pieces[6];
