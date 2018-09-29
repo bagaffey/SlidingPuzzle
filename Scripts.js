dafaa645b846442f4af39a23809deded80666a6d
Barry Gaffey <bagaffey@users.noreply.github.com>
2018-09-29 16:19:34 -0400

    Updated Scripts.js
    Focus.

--- Scripts.js
+++ Scripts.js
@@ -3,9 +3,9 @@
   "use strict";
   
   const pieces = [...document.querySelectorAll(".puzzle-game > img")];
-  const clickScore = document.getElementById("clicks");
   const soundButton = document.getElementById("toggle-audio");
   const congratulations = document.getElementsByClassName("congratulations")[0];
+  const clickScore = document.getElementById("clicks");
   let shuffledPieces = pieces;
   const hiddenPiece = pieces[6];
   let clickCount = 0;
